eb7c787e788066136023b3e2b24184e37d33e6e1
Nadezhda Bokareva <nadezhda.yureva@rt.ru>
2023-11-09 16:09:53 +0300

    commit 67
    структурирование кода

---
+++ hello first task/sandox2/src/main/java/ru/stqa/geometry/Geometry.java
@@ -0,0 +1,12 @@
+package ru.stqa.geometry;
+
+import ru.stqa.geometry.figures.Rectangle;
+import ru.stqa.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(7.0);
+        Rectangle.printRectangleArea(7.0, 3.0);
+    }
+
+}
